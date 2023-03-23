3ca247c0274d49bb08c2a1176b87410a655901f9
Maxim <panda.gtr007@gmail.com>
2023-03-23 16:26:32 +0600

    trying Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
 
-import { Header } from './components/Header';
-import { Home } from './pages/Home';
-import { Cart } from './pages/Cart';
-import { NotFound } from './pages/NotFound';
+// import { Header } from './components/Header';
+// import { Home } from './pages/Home';
+// import { Cart } from './pages/Cart';
+// import { NotFound } from './pages/NotFound';
 
 import './scss/app.scss';
 
-export const SearchContext = React.createContext({});
+// export const SearchContext = React.createContext({});
 
 export function App() {
-  const [searchValue, setSearchValue] = React.useState('');
+  // const [searchValue, setSearchValue] = React.useState('');
+
+  const count = useSelector((state) => state.filter.value);
+  const dispatch = useDispatch();
 
   return (
-    <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
       </div>
-    </SearchContext.Provider>
+    </div>
+    // <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+    //   <div className="wrapper">
+    //     <Header />
+    //     <div className="content">
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/cart" element={<Cart />} />
+    //         <Route path="*" element={<NotFound />} />
+    //       </Routes>
+    //     </div>
+    //   </div>
+    // </SearchContext.Provider>
   );
 }
